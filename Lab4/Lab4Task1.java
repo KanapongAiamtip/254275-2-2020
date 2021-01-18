183a5140213e79a833b0fb015a2381e11751cdb6
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 04:21:20 +0700

    Create Lab4Task1.java

---
+++ Lab4/Lab4Task1.java
@@ -0,0 +1,9 @@
+
+public class Lab4Task1 
+{
+    public static void main(String[] args) 
+    {
+        Person harry = new Person("Harry", 31);
+        System.out.println(harry.getName() + " is " + harry.getAge() + " years old.");
+    }
+}
