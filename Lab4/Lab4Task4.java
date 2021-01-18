6d2688bf5d14cb557667b3f5ffea7350bbd2ebd7
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 04:34:06 +0700

    Create Lab4Task4.java

---
+++ Lab4/Lab4Task4.java
@@ -0,0 +1,8 @@
+public class Lab4Task4
+{
+    public static void main(String[] args) 
+    {
+        Person4 person = new Person4("Kenneth" , 33);
+        System.out.println(person.toString());    
+    }
+}
