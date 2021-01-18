cd5a1bdb6a25ba037dfe8f2bb6c5e05147b4c742
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 00:32:41 +0700

    Create Lab2Task6A.java

---
+++ Lab2/Lab2Task6A.java
@@ -0,0 +1,12 @@
+public class Lab2Task6A
+{
+    public static void main(String[] args)
+    {
+        int x = Double(4);
+        System.out.print(x);
+    }
+    private static int Double (int z)
+    {
+        return 2*z;
+    }
+}
