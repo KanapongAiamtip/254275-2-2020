5417d54342ff5796babc2cffb10d6857d1e38871
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 02:19:35 +0700

    Create Lab3Task1.java

---
+++ Lab3/Lab3Task1.java
@@ -0,0 +1,10 @@
+public class Lab3Task1
+{
+  public static void main (String [] args)
+  {
+    String[] friends = { "Joe", "Ray", "Sam", "Paul" }; 
+    int[] ages = { 20, 21, 19, 21 };
+    int bestFriend = 2;
+    System.out.println(friends[bestFriend]+" is "+ages[bestFriend]+" years old.");
+  }
+}
