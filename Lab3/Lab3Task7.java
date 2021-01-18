e2b68e2c700a4693c319574b7d7323819c237cb9
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 02:40:24 +0700

    Create Lab3Task7.java

---
+++ Lab3/Lab3Task7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Lab3Task7
+{
+    public static void main(String args[]) 
+    {
+        Scanner in = new Scanner(System.in);
+        String inputs = "";
+        while (true)
+        {
+            inputs += "," + in.nextLine();
+            if (inputs.contains("*"))
+            {
+              break;
+            }
+        }
+        String[] arr = inputs.split(",");
+        if ((arr.length - 2) % 2 == 0)
+        {
+            for (int c = (arr.length - 2) / 2; c <= ((arr.length - 2) / 2) + 1; c++)
+            {
+                System.out.println(arr[c]);
+            }
+        }
+        else
+        {
+            System.out.print(arrOfInputs[(arr.length - 1) / 2]);
+        }
+    }
+}
