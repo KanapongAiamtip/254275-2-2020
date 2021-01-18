d0f3998921339faf22b176661bf8a5b9e911f676
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 02:34:06 +0700

    Create Lab3Task3.java

---
+++ Lab3/Lab3Task3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Lab3Task3a
+{
+    public static void main(String[]args)
+    {
+        float average1 = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Marks: ");
+        String input = sc.nextLine();
+        String number[] = input.split(" ");
+        for (int i = 0; i < number.length; i++)
+        {
+            average1 = average1 + Float.parseFloat(number[i]);
+        }
+        float average = average1 / number.length;
+        System.out.print("Your average mark is: " + average);
+    }
+}
