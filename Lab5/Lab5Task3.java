8ef912c8e8b8570ab0b5c25495ae1fd1f9d6f161
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 15:55:38 +0700

    Create Lab5Task3.java

---
+++ Lab5/Lab5Task3.java
@@ -0,0 +1,11 @@
+public class Lab5Task3
+{
+    public static void main(String[] args) 
+    {
+        Fraction3 f1 = new Fraction3(1,3);
+        Fraction3 f2 = new Fraction3(1,3);
+        Fraction3 f3 = new Fraction3(2,6);
+        System.out.println(f1.equals(f2));
+        System.out.println(f1.equals(f3));
+    }
+}
