6ceebb37baf7de4ad4f0714c977050ed9f71cf4a
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 15:36:51 +0700

    Create Fraction1.java

---
+++ Lab5/Fraction1.java
@@ -0,0 +1,15 @@
+public class Fraction1
+{
+    public int top;
+    public int bottom;
+    public Fraction1(int top, int bottom)
+    {
+        this.top = top;
+        this.bottom = bottom;
+    }
+    public String toString()
+    {
+        return top+"/"+bottom;
+    }
+}
+  
