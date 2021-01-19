72db2cb2b10dd300aa0dc492e70e7dc5c7ce3ad4
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-20 04:43:38 +0700

    Create Test1.java

---
+++ Mini Test 1/Test1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        double x = kb.nextDouble();
+        double y = kb.nextDouble();
+        System.out.println(x*y);
+    }
+
+}
