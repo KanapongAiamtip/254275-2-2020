e3083903017d8e209d0d73d056b20ab0d708de43
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-19 15:47:23 +0700

    Create Lab5Task2.java

---
+++ Lab5/Lab5Task2.java
@@ -0,0 +1,8 @@
+public class Lab5Task2
+{
+    public static void main(String[] args) 
+    {
+        Fraction2 frac = new Fraction2(1,3);
+        System.out.println(frac.toString() + " = " + frac.toDouble());
+    }
+}
