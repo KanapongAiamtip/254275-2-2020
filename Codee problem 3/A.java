8356460dad3007ec3348e7c855c55076be5d1fbf
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-20 02:56:08 +0700

    Update A.java

--- Codee problem 3/A.java
+++ Codee problem 3/A.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
+public class A {
+
+    public static void main(String[] args)
+    {
+        int[] numbers = intArray();
+        int result = sum(numbers);
+        System.out.println(result);
+    }
+    public static int sum(int[] numbers)
+    {
+        int sum =0;
+        for (int j = 0; j < numbers.length; j++)
+        {
+            sum = sum + numbers[j];
+        }
+        return sum;
+    }
+    public static String[] stringArray() 
+    {
+        Scanner reader = new Scanner(System.in);
+        String input = reader.nextLine();
+        String[] result = input.split(",");
+        return result;
+    } 
+
+    public static int[] intArray()
+    {
+        Scanner reader = new Scanner(System.in);
+        String input = reader.nextLine();
+        String[] parts = input.split(",");
+
+        int[] result = new int[parts.length];
+        for (int i = 0; i < parts.length; i++)
+        {
+            result[i] = Integer.parseInt(parts[i]);
+        }
+        return result;
+    }
+
+}
 
