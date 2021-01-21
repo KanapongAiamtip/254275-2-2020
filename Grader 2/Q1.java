3fdedffe23bf6e17fa9561a0cbac889a0d5e33d0
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-22 04:39:37 +0700

    Create Q1.java

---
+++ Grader 2/Q1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Q1
+{
+  public static void main(String[] args)
+  {
+    int in = new Scanner(System.in).nextInt();
+    for(int i = 1; i <= in; i++)
+    {
+     System.out.print(i * i + " "); 
+    }
+  }
+}
