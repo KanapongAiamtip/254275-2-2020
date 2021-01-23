6c9033d75c50908b67347914b08084d061478a4a
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-24 02:52:40 +0700

    Create Lab6Task1.java

---
+++ Lab 6/Lab6Task1.java
@@ -0,0 +1,9 @@
+public class Lab6Task1 
+{
+    public static void main(String[] args) 
+    {
+        int[] someNumner = { 5, 3, 8, 8 };
+        int sum = Calculator1.sum(someNumner);
+        System.out.println("Sum is " + sum);
+    }
+}
