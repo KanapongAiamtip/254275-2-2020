75ddda9fa4b3735cecea1ca639d22c62c48118e6
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-24 02:54:26 +0700

    Create Lab6Task2.java

---
+++ Lab 6/Lab6Task2.java
@@ -0,0 +1,9 @@
+public class Lab6Task2 
+{
+    public static void main(String[] args) 
+    {
+        int[] someNumner = { 5, 3, 8, 8 };
+        double avg = Calculator2.average(someNumner);
+        System.out.println("Average is " + avg);
+    }
+}
