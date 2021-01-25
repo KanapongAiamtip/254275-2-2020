643be2a47bf4ccf3935ab9b89f97ab1b30dc8732
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-26 03:11:31 +0700

    Update Calculator6.java

--- Lab 6/Calculator6.java
+++ Lab 6/Calculator6.java
@@ -23,7 +23,7 @@ public class Calculator6 {
     
     public static double sum (int[] list1, double[] list2)
     {
-        return Calculator6.sum(list1)+Calculator.sum(list2);
+        return Calculator6.sum(list1)+Calculator6.sum(list2);
     }
 
     
