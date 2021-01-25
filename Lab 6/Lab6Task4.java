8c1f77fad84a69df00d636b6755ffb3eba7c67ff
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-26 03:03:08 +0700

    Create Lab6Task4.java

---
+++ Lab 6/Lab6Task4.java
@@ -0,0 +1,10 @@
+public class Lab6Task4 
+{
+    public static void main(String[] args) 
+    {
+        double[] number = { 2.3, 5.4, 7.1, 9.6 };
+        double avg = Calculator4.sum(number);
+        System.out.println("Average double is " + avg);
+
+    }
+}
