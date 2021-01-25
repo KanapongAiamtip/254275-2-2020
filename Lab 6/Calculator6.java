e62cf58272aaca6d1b63682ca66283dd6416f8fb
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-26 03:10:16 +0700

    Create  Calculator6.java

---
+++ Lab 6/Calculator6.java
@@ -0,0 +1,30 @@
+public class Calculator6 {
+
+    
+    public static int sum(int[] numbers) 
+    {
+        int sum = 0;
+            for (int i = 0; i < numbers.length; i++) 
+            {
+                sum = sum + numbers[i];
+            }
+            return sum;
+    }
+    
+    public static double sum(double[] numbers) 
+    {
+        double sum = 0;
+            for (int i = 0; i < numbers.length; i++) 
+            {
+                sum = sum + numbers[i];
+            }
+            return sum;
+    }
+    
+    public static double sum (int[] list1, double[] list2)
+    {
+        return Calculator6.sum(list1)+Calculator.sum(list2);
+    }
+
+    
+}
