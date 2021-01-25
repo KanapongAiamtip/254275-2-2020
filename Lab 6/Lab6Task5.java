23b6bdf6de72fba35347b4882272db3bddf39081
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-26 03:10:30 +0700

    Update Lab6Task5.java

--- Lab 6/Lab6Task5.java
+++ Lab 6/Lab6Task5.java
@@ -4,6 +4,6 @@ public class Lab6Task5
     {
         int [] a  = {5,3,8,8};
         double[] b = { 2.3, 5.4, 7.1, 9.6 };
-        System.out.println(" sum int and double is " + Calculator.sum(a,b));
+        System.out.println(" sum int and double is " + Calculator5.sum(a,b));
     }
 }
