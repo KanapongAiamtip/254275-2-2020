813f1b53daf4236452da0a52cf5319812c1c358c
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-26 03:00:37 +0700

    Create Calculator3.java

---
+++ Lab 6/Calculator3.java
@@ -0,0 +1,19 @@
+public class Calculator3 {
+    public static int sum(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        return sum;
+    }
+
+    public static double sum(double[] numbers) {
+        double sum = 0;
+        double average = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        return sum;
+    }
+
+   
