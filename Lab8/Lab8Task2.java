6cfaba7c34be3737150f1dfdb710422d81fd0387
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-02-22 18:43:10 +0700

    Create Lab8Task2.java

---
+++ Lab8/Lab8Task2.java
@@ -0,0 +1,9 @@
+public class Lab8Task2 
+{
+    public static void main(String[] args) 
+    {
+        Rectangle blueRect = new Rectangle("blue", 10, 20);
+        System.out.println("Width = " + blueRect.getWidth() + ", Height = " + blueRect.getHeight());
+        System.out.println(blueRect.toString());
+    }    
+}
