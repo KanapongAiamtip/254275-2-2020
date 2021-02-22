8d4d13afbb1508db334b1044363a1db79f8fb596
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-02-22 18:56:30 +0700

    Create Lab8Task4.java

---
+++ Lab8/Lab8Task4.java
@@ -0,0 +1,11 @@
+public class Lab8Task4 
+{
+    public static void main(String[] args) 
+    {
+        Rectangle shapeA = new Rectangle("pink", 3 ,4);
+        Circle shapeB = new Circle("yellow", 7);
+        
+        System.out.println(shapeA.getWidth());
+        System.out.println(shapeB.getRadius());
+    }
+}
