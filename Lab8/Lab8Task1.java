20515dc3dccbdb901220a86e702c69939cd8d0cc
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-02-22 17:30:24 +0700

    Create Lab8Task1.java

---
+++ Lab8/Lab8Task1.java
@@ -0,0 +1,8 @@
+public class Lab8Task1 
+{
+    public static void main(String[] args) 
+    {
+        Shape colour = new Shape("red");
+        System.out.println(colour.toString());
+    }    
+}
