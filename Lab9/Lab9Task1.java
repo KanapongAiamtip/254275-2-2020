b5f33a49986fdc16825fee091150cee1e6cef8d7
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-02-22 19:26:28 +0700

    Create Lab9Task1.java

---
+++ Lab9/Lab9Task1.java
@@ -0,0 +1,11 @@
+public class Lab9Task1 
+{
+
+    public static void main(String[] args) 
+    {
+        Place nu = new Place("Naresuan University", "99/9 Thapho, Muang", "Phitsanulok");
+        System.out.println("Place name: " + nu.getName());
+        System.out.println("Address: " + nu.getAddress());
+        System.out.println("Province: " + nu.getProvince())
+    }
+}
