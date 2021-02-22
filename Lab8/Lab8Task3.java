1b8ee18022ac115ac500c4c2c40331852f85914a
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-02-22 18:55:26 +0700

    Create Lab8Task3.java

---
+++ Lab8/Lab8Task3.java
@@ -0,0 +1,9 @@
+public class Lab8Task3 
+{
+    public static void main(String[] args) 
+    {
+        Circle greenCircle = new Circle("green", 5);
+        System.out.println("Radius = " + greenCircle.getRadius());
+        System.out.println(greenCircle.toString());
+    }
+}
