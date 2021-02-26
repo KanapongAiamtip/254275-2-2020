c03896aa41c0c83a0cf915ea64a85650a6446d37
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-02-26 10:39:43 +0700

    Create Lab11Task1.java

---
+++ Lab11/Lab11Task1.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Lab11Task1
+{
+  public static void main(String[] args)
+  {
+    JFrame frame = new JFrame();
+    {
+      frame.setTitle("My Application");
+      frame.setSize(300,200);
+      frame.setLocation(10, 200);
+      frame.setVisible(true);
+    }
+  }
+}
