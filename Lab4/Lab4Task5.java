14d70206cde1d5237a8775a7108d2bebf36e0eeb
Louis Thanawin <75485361+KanapongAiamtip@users.noreply.github.com>
2021-11-02 23:57:21 +0700

    Update Lab4Task5.java

--- Lab4/Lab4Task5.java
+++ Lab4/Lab4Task5.java
@@ -2,9 +2,9 @@ public class Lab4Task5
 {
     public static void main(String[] args) 
     {
-        Person4 personA = new Person4("Andrew" , 12);
-        Person4 personB = new Person4("Beryl" , 12);
-        Person4 personC = new Person4("Andrew" , 12);
+        Person4 personA = new Person5("Andrew" , 12);
+        Person4 personB = new Person5("Beryl" , 12);
+        Person4 personC = new Person5("Andrew" , 12);
 
         if (personA.equals(personB))
         {
