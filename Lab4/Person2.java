931dcd1a45817a5bb9800fb368a65a689ab24899
Louis Thanawin <75485361+KanapongAiamtip@users.noreply.github.com>
2021-11-02 23:55:54 +0700

    Update Person2.java

--- Lab4/Person2.java
+++ Lab4/Person2.java
@@ -2,7 +2,7 @@ public class Person2
 {
     private String name;
     private int age;
-    public Person1(String name,int age)
+    public Person2(String name,int age)
     {
         this.name = name;
         this.age = age;
